660ce3072b7090f24c44f76f0e0362d771c35997
Moudi <mjaderi97@gmail.com>
2025-02-20 18:25:28 +1100

    Added Invoice Table with Dummy Data

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,3 +1,41 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PlusIcon } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+import InvoiceTable from "./_components/InvoiceTable";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+  description: "Invoices page",
+};
+
 export default function Page() {
-  return <div>invoices page</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle>Invoices</CardTitle>
+            <CardDescription>Manage your invoices</CardDescription>
+          </div>
+          <Button asChild variant="outline" className="flex items-center gap-2">
+            <Link href="/invoices/new">
+              <PlusIcon className="size-5" />
+              Create Invoice
+            </Link>
+          </Button>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <InvoiceTable />
+      </CardContent>
+    </Card>
+  );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard page",
+};
+
 export default async function Page() {
-  return <div>
-    hello from dashboard
-  </div>;
+  return <div>hello from dashboard</div>;
 }
