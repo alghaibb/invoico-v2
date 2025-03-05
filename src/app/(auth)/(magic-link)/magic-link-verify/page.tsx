a40d365c3725ba4025c26d27685c816af1818787
Moudi <mjaderi97@gmail.com>
2025-03-06 06:18:33 +1100

    Wrap VerifyMagicLinkPage and ResetPasswordPage components in Suspense for improved loading state handling

--- src/app/(auth)/(magic-link)/magic-link-verify/page.tsx
+++ src/app/(auth)/(magic-link)/magic-link-verify/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
 import VerifyMagicLinkPage from "./VerifyMagicLinkPage";
 
 export const metadata: Metadata = {
@@ -6,5 +7,9 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <VerifyMagicLinkPage />;
+  return (
+    <Suspense fallback={<p>loading...</p>}>
+      <VerifyMagicLinkPage />
+    </Suspense>
+  );
 }
--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,15 @@
-import ResetPasswordPage from "./ResetPasswordPage";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import ResetPasswordPage from "./ResetPasswordPage";
 
 export const metadata: Metadata = {
   title: "Reset Password",
 };
 
 export default function Page() {
-  return <ResetPasswordPage />;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ResetPasswordPage />
+    </Suspense>
+  );
 }
